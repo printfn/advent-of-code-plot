7f977035bf161122b9767f0bf4fc6ddf53a680bb
printfn <printfn@users.noreply.github.com>
2020-12-25 23:47:52 +1300

    Add node.js script to update leaderboard data

---
+++ update-data.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const https = require('https');
+
+if (process.argv.length < 3) {
+    console.error('Please specify a session cookie.');
+    process.exit(1);
+}
+let session = process.argv[2];
+
+const req = https.request({
+    hostname: 'adventofcode.com',
+    port: 443,
+    path: '/2020/leaderboard/private/view/225901.json',
+    method: 'GET',
+    headers: {
+        Cookie: `session=${session}`
+    }
+}, res => {
+    if (res.statusCode != 200) {
+        console.error(`Invalid status code: ${res.statusCode}`);
+        console.log(res.headers);
+        process.exit(1);
+    }
+
+    res.on('data', data => {
+        let content = `loadData(${data});`
+        fs.writeFile('data.js', content, err => {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+            // file written successfully
+        });
+    });
+});
+
+req.on('error', error => {
+    console.error(error);
+});
+
+req.end();
